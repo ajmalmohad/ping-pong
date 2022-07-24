a5848421f4cd666452de6d4be50b015320ac4ff1
ajmalmohad <ajmalajmal.2016@gmail.com>
2022-07-24 11:00:32 +0530

    Collision

--- script.js
+++ script.js
@@ -1,55 +1,37 @@
 /**
- * Support
- */
-function checkCompatibilty() {
-    if(window.innerWidth < 560){
-        document.getElementsByClassName('small-device')[0].style.display = "flex";
-        document.getElementsByClassName('canvas-container')[0].style.display = "none";
-    }else{
-        document.getElementsByClassName('small-device')[0].style.display = "none";
-        document.getElementsByClassName('canvas-container')[0].style.display = "flex";
-    }
-}
-checkCompatibilty()
-window.addEventListener('resize', ()=>{checkCompatibilty()})
-
-
-
-/**
  * Canvas and Context
  */
 const canvas = document.getElementById('container')
 const context = canvas.getContext("2d")
 
 
-
 /**
  * Objects
  */
 const ball = {
-    radius: 10,
-    positionX: 100,
-    positionY: 200,
+    radius: 8,
+    positionX: canvas.width / 2 + 8,
+    positionY: canvas.height / 2 + 8,
     velocityX: 1,
-    velocityY: 1,
+    velocityY: -1,
     color: 'white'
 }
 
 const leftPlayer = {
-    height: 40,
+    height: 100,
     width: 10,
     positionX: 10,
-    positionY: canvas.height / 2 - 40 / 2,
+    positionY: canvas.height / 2 - 100 / 2,
     color: 'white',
     player: 'left',
     speed: 1
 }
 
 const rightPlayer = {
-    height: 40,
+    height: 100,
     width: 10,
     positionX: canvas.width - 20,
-    positionY: canvas.height / 2 - 40 / 2,
+    positionY: canvas.height / 2 - 100 / 2,
     color: 'white',
     player: 'right',
     speed: 1
@@ -82,6 +64,7 @@ function drawLeftPlayer() {
     context.fillStyle = leftPlayer.color;
     context.rect(leftPlayer.positionX, leftPlayer.positionY, leftPlayer.width, leftPlayer.height);
     context.fill();
+    context.closePath();
 }
 
 function drawRightPlayer() {
@@ -89,16 +72,28 @@ function drawRightPlayer() {
     context.fillStyle = rightPlayer.color;
     context.rect(rightPlayer.positionX, rightPlayer.positionY, rightPlayer.width, rightPlayer.height);
     context.fill();
+    context.closePath();
 }
+
+
+function drawBall() {
+    context.beginPath();
+    context.fillStyle = ball.color;
+    context.arc(ball.positionX, ball.positionY, ball.radius, 0, Math.PI * 2);
+    context.fill();
+    context.closePath();
+}
+
 
 function drawAll() {
     context.clearRect(0, 0, canvas.width, canvas.height);
     drawLeftPlayer()
     drawRightPlayer()
+    drawBall()
 }
 
 
-function updateKeyPresses(){
+function updateKeyPresses() {
     if (keyPressed['W']) {
         if (leftPlayer.positionY > 0) {
             leftPlayer.positionY -= leftPlayer.speed;
@@ -122,7 +117,20 @@ function updateKeyPresses(){
 }
 
 function updateStates() {
-    
+    if ((ball.positionY + ball.radius) === canvas.height || (ball.positionY - ball.radius) === 0) {
+        ball.velocityY = -ball.velocityY;
+    }
+
+    if ((ball.positionX + ball.radius) === (canvas.width - (rightPlayer.width + 10)) ||
+        (ball.positionX - ball.radius) === (0 + (rightPlayer.width + 10))) {
+        ball.velocityX = -ball.velocityX;
+    }
+
+    console.log(ball.positionX, canvas.width);
+
+
+    ball.positionX += ball.velocityX;
+    ball.positionY += ball.velocityY;
 }
 
 
@@ -145,7 +153,7 @@ document.addEventListener('keydown', (event) => {
     if (code === 'ArrowDown') {
         keyPressed['Down'] = true;
     }
-    
+
 }, false);
 
 
@@ -182,3 +190,36 @@ function gameLoop() {
 }
 
 requestAnimationFrame(gameLoop);
+
+
+
+
+/**
+ * Support
+ */
+function updateDefault() {
+    canvas.width = window.innerWidth * 0.6
+    canvas.height = window.innerHeight * 0.8
+
+    ball.positionX = canvas.width / 2 + ball.radius
+    ball.positionY = canvas.height / 2 + ball.radius
+
+    leftPlayer.positionY = canvas.height / 2 - leftPlayer.height / 2
+
+    rightPlayer.positionX = canvas.width - (rightPlayer.width + 10)
+    rightPlayer.positionY = canvas.height / 2 - rightPlayer.height / 2
+}
+
+function resizeHandler() {
+    if (window.innerWidth < 560) {
+        document.getElementsByClassName('small-device')[0].style.display = "flex";
+        document.getElementsByClassName('canvas-container')[0].style.display = "none";
+    } else {
+        document.getElementsByClassName('small-device')[0].style.display = "none";
+        document.getElementsByClassName('canvas-container')[0].style.display = "flex";
+    }
+
+    updateDefault()
+}
+resizeHandler()
+window.addEventListener('resize', () => { resizeHandler() })
